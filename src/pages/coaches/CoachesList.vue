de4dcce7e5290976f1c5fc7b63cfb8d01f8b55df
Alex <cigan@ua.fm>
2021-01-09 18:58:15 +0200

    Add getters for coaches's state and add computed into CoacheList to show a list of coaches

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,8 +7,24 @@
       <button>Refresh</button>
       <router-link to="/register">Register as a Coach</router-link>
     </div>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filterdCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
+    <h3 v-else>Sorry, there is no coach now!</h3>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filterdCoaches() {
+      return this.$store.getters['coaches/coaches']; // becouse of using namespaced
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    }
+  }
+};
+</script>
