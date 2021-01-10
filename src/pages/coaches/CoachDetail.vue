5a77d8783a5e67f0905e29c3e54d177ae660355e
Alex <cigan@ua.fm>
2021-01-10 17:38:23 +0200

    Make a component CoachDetail to display info about a particular coach

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,6 +1,60 @@
 <template>
-  DETAILS FOR COACH
-  <router-link to="/coaches/c1/contact">Contract</router-link>
-  <!-- to show content of ContactCoach, becouse ContactCoach is a child of CoachDetail -->
-  <router-view></router-view>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>$ {{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button link :to="contactLink">Contact</base-button>
+      </header>
+      <!-- to show content of ContactCoach, becouse ContactCoach is a child of CoachDetail -->
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge
+        v-for="area in selectedCoach.areas"
+        :key="area"
+        :title="area"
+        :type="area"
+      ></base-badge>
+      <p>{{ description }}</p>
+    </base-card>
+  </section>
 </template>
+
+<script>
+export default {
+  props: ['id'],
+  data() {
+    return {
+      selectedCoach: null
+    };
+  },
+  created() {
+    this.selectedCoach = this.$store.getters['coaches/coaches'].find(
+      coach => coach.id === this.id
+    );
+  },
+  computed: {
+    fullName() {
+      return this.selectedCoach.firstName + ' ' + this.selectedCoach.lastName;
+    },
+    contactLink() {
+      return this.$route.path + '/' + this.id + '/contact';
+    },
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+
+    description() {
+      return this.selectedCoach.description;
+    }
+  }
+};
+</script>
