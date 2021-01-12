7a6d59a73f03d2ec1fb50c564adf7af06b6ed5dd
Alex <cigan@ua.fm>
2021-01-12 16:59:03 +0200

    Add fetch() to send coach data to the server (Firebase)

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,29 @@
 export default {
-    registerCoach(context, data) {
+    async registerCoach(context, data) {
+        const userId = context.rootGetters.userId;
         const coachData = {
-            id: context.rootGetters.userId,
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
             hourlyRate: data.rate,
             areas: data.areas
         }
-        context.commit('registerCoach', coachData)
+
+        // async/await in order not to use .then()/.catch()
+        const response = await fetch(`https://vue-http-demo-5b114-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(coachData)
+        });
+
+        // consts responseData = await response.json();
+
+        if (!response.ok) {
+            // error ...
+        }
+
+        context.commit('registerCoach', {
+            ...coachData,
+            id: userId
+        })
     }
 };
