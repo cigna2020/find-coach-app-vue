bdec6f7b5f72e074497434dabfe6be35db3152e0
Alex <cigan@ua.fm>
2021-01-14 16:32:41 +0200

    Create navigation guard to protect direct input a path (/requests, /register)

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import ContactCoach from './pages/requests/ContactCoach';
 import RequestsReceived from './pages/requests/RequestsReceived';
 import UserAuth from './pages/auth/UserAuth';
 import NotFound from './pages/NotFound';
+import store from './store/index';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -17,11 +18,22 @@ const router = createRouter({
             path: '/coaches/:id', component: CoachDetail, props: true,
             children: [{path: 'contact', component: ContactCoach}] // /coaches/c1/contact'
         },
-        {path: '/register', component: CoachRegistration},
-        {path: '/requests', component: RequestsReceived},
-        {path: '/auth', component: UserAuth},
+        // meta uses to protect link, look router.beforeEach below
+        {path: '/register', component: CoachRegistration, meta: {requiresAuth: true}},
+        {path: '/requests', component: RequestsReceived, meta: {requiresAuth: true}},
+        {path: '/auth', component: UserAuth, meta: {requiresUnauth: true}},
         {path: '/:notFound(.*)', component: NotFound},
     ]
 });
+
+router.beforeEach(function (to, _, next) {  // _ - from, does't use in this case
+    if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+        next('/auth');
+    } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+        next('/coaches');
+    } else {
+        next();
+    }
+})
 
 export default router;
